d8f2357386c61712a03d2a4f55686261ce12c20a
anmol dhiman <anmoldhiman7111@gmail.com>
2023-01-13 10:16:56 +0530

    using c# with electron

--- basic/main.js
+++ basic/main.js
@@ -1,49 +1,55 @@
-const { app, BrowserWindow, shell } = require("electron");
-const path = require("path");
-let win;
+// main.js
+const { app, BrowserWindow } = require("electron");
+const edge = require("electron-edge-js");
+
+let mainWindow;
 
 function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-
-  // load the index.html of the app.
-  win.loadFile("index.html");
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  win.on("closed", () => {
-    win = null;
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
   });
 }
 
 app.on("ready", createWindow);
 
-app.on("activate", () => {
-  if (win === null) {
-    createWindow();
-  }
-});
-
-app.on("window-all-closed", () => {
+app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on("will-finish-launching", () => {
-  app.on("open-url", (event, url) => {
-    event.preventDefault();
-    const gitBashPath = path.join(
-      process.env.LOCALAPPDATA,
-      "Programs/Git/git-bash.exe"
-    );
-    shell.openItem(gitBashPath);
-  });
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+const exePath = "C:\\path\\to\\exe\\file.exe";
+const start = edge.func(`
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        public async Task<object> Invoke(string exePath)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    UseShellExecute = true,
+                    CreateNoWindow = false
+                }
+            };
+            process.Start();
+            return null;
+        }
+    }
+`);
+
+start(exePath, (error, result) => {
+  if (error) throw error;
 });
